698e2f440b1324cb0fa1b1e4e527ac0a6ebdbbc0
Hannah Ahn <ahn.hannah.dev@gmail.com>
2024-02-07 19:28:15 +0900

    [#3] feat: 수업 예약 페이지에서 나가기 버튼 생성

--- src/components/Header/BackButton.tsx
+++ src/components/Header/BackButton.tsx
@@ -1,5 +1,12 @@
 const BackButton = () => {
-  return <div></div>
+  return (
+    <button className='flex items-center gap-x-3'>
+      <i className='material-symbols-outlined text-[20px] text-primary'>
+        chevron_left
+      </i>
+      <div className='text-primary text-xs'>나가기</div>
+    </button>
+  )
 }
 
 export default BackButton
