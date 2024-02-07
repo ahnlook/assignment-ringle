e60d92898d4eb71fe28257e1299d8ecba1ac5b5b
Hannah Ahn <ahn.hannah.dev@gmail.com>
2024-02-07 21:04:46 +0900

    [#3] feat: 수업 예약 헤더에서 수업권 변경 기능

--- src/components/Header/LessonTicketButton.tsx
+++ src/components/Header/LessonTicketButton.tsx
@@ -1,5 +1,35 @@
+import { useDispatch, useSelector } from 'react-redux'
+
+import { lessonBookingUiActions } from '../../store/lessonBookingUi-slice'
+import { RootState } from '../../store'
+import { LessonTicket } from '../../type/lessonBooking'
+
 const LessonTicketButton = () => {
-  return <div></div>
+  const dispatch = useDispatch()
+  const selectedTicket = useSelector((state: RootState) =>
+    state.lessonTickets.find(ticket => ticket.isSelected === true)
+  )
+  const { type, name, unusedTickets } = selectedTicket as LessonTicket
+
+  const handleClick = () => {
+    dispatch(lessonBookingUiActions.openLessonTicketModal())
+  }
+
+  return (
+    <button
+      className='min-w-[360px] max-h-10 flex items-center justify-between px-4 py-2 bg-white text-sm border border-gray-300 rounded-md'
+      onClick={() => handleClick()}
+    >
+      <div className='flex items-center gap-x-2'>
+        <div className='px-2 bg-green-500 text-green-600 text-xs rounded'>
+          {type}
+        </div>
+        <div className='font-medium'>{name}</div>
+        <div>({unusedTickets}회 남음)</div>
+      </div>
+      <i className='material-symbols-outlined text-[19px]'>expand_more</i>
+    </button>
+  )
 }
 
 export default LessonTicketButton
