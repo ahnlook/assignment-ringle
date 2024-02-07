291725b94bcc7f3a43dad08ea3a6d59e8b56eae9
Hannah Ahn <ahn.hannah.dev@gmail.com>
2024-02-07 18:30:12 +0900

    [#1] feat: 수업권 slice 정의

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,13 +1,15 @@
 import { configureStore, createListenerMiddleware } from '@reduxjs/toolkit'
 
 import lessonBookingUiSlice from './lessonBookingUi-slice'
+import lessonTicketsSlice from './lessonTickets-slice'
 
 export const listenerMiddleware = createListenerMiddleware()
 
 const store = configureStore({
   reducer: {
+    lessonBookingUi: lessonBookingUiSlice.reducer,
+    lessonTickets: lessonTicketsSlice.reducer
     // 여기에 다른 리듀서들을 추가
-    lessonBookingUi: lessonBookingUiSlice.reducer
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().prepend(listenerMiddleware.middleware),
---
+++ src/store/lessonTickets-slice.ts
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+import { LessonTicket } from '../type/lessonBooking'
+
+const DUMMY_LESSON_TICKETS: LessonTicket[] = [
+  {
+    id: 1,
+    type: '20분',
+    name: '1:1 회화(20분)',
+    remainingPeriod: 30,
+    unusedTickets: 5,
+    isSelected: true
+  },
+  {
+    id: 2,
+    type: '40분',
+    name: '1회 패키지',
+    remainingPeriod: 45,
+    unusedTickets: 1,
+    isSelected: false
+  }
+]
+
+const lessonTicketSlice = createSlice({
+  name: 'lessonTicket',
+  initialState: DUMMY_LESSON_TICKETS,
+  reducers: {
+    selectLessonTicket(state, action) {
+      const selectedId = action.payload
+      state.forEach(ticket => {
+        ticket.isSelected = ticket.id === selectedId
+      })
+    }
+  }
+})
+
+export const lessonTicketActions = lessonTicketSlice.actions
+export default lessonTicketSlice
