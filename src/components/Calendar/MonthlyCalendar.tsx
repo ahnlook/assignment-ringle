8c8709ae836c3b57fa29646e2cc2f7bb84626f0a
Hannah Ahn <ahn.hannah.dev@gmail.com>
2024-02-08 11:05:44 +0900

    [#5] feat: day-picker를 활용한 달력 생성

--- src/components/Calendar/MonthlyCalendar.tsx
+++ src/components/Calendar/MonthlyCalendar.tsx
@@ -1,5 +1,45 @@
+import { DayPicker } from 'react-day-picker'
+import 'react-day-picker/dist/style.css'
+import { ko } from 'date-fns/locale'
+import { useDispatch, useSelector } from 'react-redux'
+import { lessonBookingDateActions } from '../../store/lessonBookingDate-slice'
+import { RootState } from '../../store'
+
+const today = new Date()
 const MonthlyCalendar = () => {
-  return <div></div>
+  const dispatch = useDispatch()
+  const range = useSelector(
+    (state: RootState) => state.lessonBookingDate.currentWeek
+  )
+
+  const handleDayClick = (day: Date) => {
+    dispatch(lessonBookingDateActions.setWeek(day))
+  }
+
+  return (
+    <>
+      <style>{css}</style>
+      <div className='p-2.5'>
+        <DayPicker
+          mode='range'
+          selected={range}
+          defaultMonth={today}
+          fromDate={today}
+          locale={ko}
+          showOutsideDays
+          onDayClick={handleDayClick}
+          modifiersClassNames={{
+            selected: 'my-selected'
+          }}
+        />
+      </div>
+    </>
+  )
 }
 
 export default MonthlyCalendar
+
+const css = `.my-selected {
+  color: black;
+  background-color: #E4E7F4;
+}`
