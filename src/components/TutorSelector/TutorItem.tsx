59f239cb6431e89390c398df0728639abf0e9015
Hannah Ahn <ahn.hannah.dev@gmail.com>
2024-02-09 17:12:55 +0900

    [#5] feat: 튜터 선택을 위한 컴포넌트 생성

---
+++ src/components/TutorSelector/TutorItem.tsx
@@ -0,0 +1,51 @@
+import { useDispatch, useSelector } from 'react-redux'
+
+import { lessonBookingActions } from '../../store/lessonBooking-slice'
+import { RootState } from '../../store'
+import { Tutor } from '../../type/tutor'
+
+const TutorItem = ({ tutor }: { tutor: Tutor }) => {
+  const { id, name, university, major, acceptanceRate, tag } = tutor
+  const dispatch = useDispatch()
+  const date = useSelector(
+    (state: RootState) => state.lessonBooking.selectedDate
+  )
+  const ticketId = useSelector(
+    (state: RootState) => state.lessonBooking.selectedTicketId
+  )
+  const ticket = useSelector((state: RootState) =>
+    state.lessonTicket.tickets.find(ticket => ticket.id === ticketId)
+  )
+
+  const onTutorClick = () => {
+    if (!date) return
+    if (!ticketId) return
+    if (!ticket) return
+    if (ticket.unusedTickets <= 0) return alert('수업권이 부족합니다')
+
+    dispatch(
+      lessonBookingActions.booking({
+        date,
+        ticketId,
+        tutorId: id
+      })
+    )
+  }
+
+  return (
+    <button className='w-full p-4 text-left border-b' onClick={onTutorClick}>
+      <div className='text-h-2'>{name}</div>
+      <div className='text-h-5'>{university}</div>
+      <div className='text-gray-500 text-sm'>{major}</div>
+      <div className=' text-sm'>
+        <span className='text-gray-500'>수락률 </span>
+        <span>{acceptanceRate}%</span>
+      </div>
+      <div className='inline-block px-1.5 bg-gray-100 text-gray-700 text-h-6 rounded-md'>
+        {tag}
+      </div>
+    </button>
+  )
+}
+
+export default TutorItem
---
+++ src/components/TutorSelector/index.tsx
@@ -0,0 +1,28 @@
+import { useSelector } from 'react-redux'
+
+import { RootState } from '../../store'
+import TutorItem from './TutorItem'
+import TutorSelectionMessage from './TutorSelectionMessage'
+
+const TutorSelector = () => {
+  const tutors = useSelector((state: RootState) => state.tutor.tutors)
+  const isDateSelected = useSelector((state: RootState) =>
+    Boolean(state.lessonBooking.selectedDate)
+  )
+
+  return (
+    <div className='h-screen w-[347px] ml-2 border-x'>
+      {!isDateSelected ? (
+        <TutorSelectionMessage />
+      ) : (
+        <div>
+          {tutors.map(tutor => (
+            <TutorItem key={tutor.id} tutor={tutor} />
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default TutorSelector
