52334391e2cfc5e61a47b1629ad7f52151a2c677
Hannah Ahn <ahn.hannah.dev@gmail.com>
2024-02-09 17:12:11 +0900

    [#5] feat: 수업 시간 선택 안내 문구를 위한 컴포넌트 생성

---
+++ src/components/TutorSelector/TutorSelectionMessage.tsx
@@ -0,0 +1,12 @@
+const TutorSelectionMessage = () => {
+  return (
+    <div className='flex items-center justify-center h-full'>
+      <div className='w-9/12 flex flex-col p-5 text-gray-500 text-center border rounded-md'>
+        <span>캘린더에서 원하시는 시간을 눌러</span>
+        <span>수업을 신청해주세요</span>
+      </div>
+    </div>
+  )
+}
+
+export default TutorSelectionMessage
