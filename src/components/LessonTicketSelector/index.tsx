16e715df81a24d84a46c7571dd0ccb22f4f0f59b
Hannah Ahn <ahn.hannah.dev@gmail.com>
2024-02-10 17:02:24 +0900

    [#6] feat: 모달 컴포넌트 분리

--- src/components/LessonTicketSelector/index.tsx
+++ src/components/LessonTicketSelector/index.tsx
@@ -1,53 +1,37 @@
-import { useRef } from 'react'
-import { createPortal } from 'react-dom'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { lessonBookingUiActions } from '../../store/lessonBookingUi-slice'
 import { RootState } from '../../store'
 import { LessonTicket } from '../../type/lessonTicket'
-import useOutsideClick from '../../hooks/useOutsideClick'
-import Dimmed from './Dimmed'
 import LessonTicketItem from './LessonTicketItem'
+import Modal from '../Modal'
 
 const LessonTicketSelector = () => {
   const dispatch = useDispatch()
-  const modalRef = useRef<HTMLDivElement>(null)
   const { tickets } = useSelector((state: RootState) => state.lessonTicket)
-  useOutsideClick(modalRef, () => {
-    dispatch(lessonBookingUiActions.closeLessonTicketModal())
-  })
 
-  return createPortal(
-    <>
-      <Dimmed />
-      <div className='fixed inset-0 h-screen flex items-center justify-center opacity-100 translate-y-0'>
-        <div
-          className='w-full max-w-[724px] p-8 flex flex-col gap-y-4 rounded-2xl bg-white shadow-lg'
-          ref={modalRef}
-        >
-          <div className='flex justify-between'>
-            <div className=''>
-              <p className='text-h-2'>수업권 선택</p>
-            </div>
-            <div className='w-5 h-5'>
-              <button
-                onClick={() => {
-                  dispatch(lessonBookingUiActions.closeLessonTicketModal())
-                }}
-              >
-                <i className='material-symbols-outlined'>close</i>
-              </button>
-            </div>
-          </div>
-          <div className='flex flex-col gap-y-4'>
-            {tickets.map((ticket: LessonTicket) => (
-              <LessonTicketItem key={ticket.id} ticket={ticket} />
-            ))}
-          </div>
+  const handleClose = () => {
+    dispatch(lessonBookingUiActions.closeLessonTicketModal())
+  }
+
+  return (
+    <Modal onClose={handleClose}>
+      <div className='flex justify-between'>
+        <div className=''>
+          <p className='text-h-2'>수업권 선택</p>
+        </div>
+        <div className='w-5 h-5'>
+          <button onClick={handleClose}>
+            <i className='material-symbols-outlined'>close</i>
+          </button>
         </div>
       </div>
-    </>,
-    document.body
+      <div className='flex flex-col gap-y-4'>
+        {tickets.map((ticket: LessonTicket) => (
+          <LessonTicketItem key={ticket.id} ticket={ticket} />
+        ))}
+      </div>
+    </Modal>
   )
 }
 
---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,33 @@
+import { useRef } from 'react'
+import { createPortal } from 'react-dom'
+
+import Dimmed from './Dimmed'
+import useOutsideClick from '../../hooks/useOutsideClick'
+
+const Modal = ({
+  onClose,
+  children
+}: {
+  onClose: () => void
+  children: React.ReactNode
+}) => {
+  const modalRef = useRef<HTMLDivElement>(null)
+  useOutsideClick(modalRef, onClose)
+
+  return createPortal(
+    <>
+      <Dimmed />
+      <div className='fixed inset-0 h-screen flex items-center justify-center opacity-100 translate-y-0'>
+        <div
+          className='w-full max-w-[724px] p-8 flex flex-col gap-y-4 rounded-2xl bg-white shadow-lg'
+          ref={modalRef}
+        >
+          {children}
+        </div>
+      </div>
+    </>,
+    document.body
+  )
+}
+
+export default Modal
