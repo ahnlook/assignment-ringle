9aee83b10b9d257c97a53ab10b548d1bb57ddf1e
Hannah Ahn <ahn.hannah.dev@gmail.com>
2024-02-11 18:34:59 +0900

    refactor: timeSlots 함수 분리

--- src/components/Scheduler/DayColumn.tsx
+++ src/components/Scheduler/DayColumn.tsx
@@ -1,4 +1,4 @@
-import { format, isToday as checkIsToday, isAfter, startOfDay } from 'date-fns'
+import { format, isAfter, startOfDay, isSameDay } from 'date-fns'
 import { ko } from 'date-fns/locale'
 
 import HalfHourBlock from './HalfHourBlock'
@@ -7,17 +7,10 @@ interface DayColumnProps {
   date: Date
 }
 
-const DayColumn = ({ date }: DayColumnProps) => {
-  const dayOfWeek = format(date, 'eee', { locale: ko })
-  const dayOfMonth = format(date, 'd')
-  const isToday = checkIsToday(date)
-  const isHoliday = dayOfWeek === '일' || dayOfWeek === '토'
-  const now = new Date()
-
-  const timeSlots = Array.from({ length: 48 }, (_, i) => {
+const calculateTimeSlots = (date: Date, now: Date) =>
+  Array.from({ length: 48 }, (_, i) => {
     const hour = Math.floor(i / 2)
     const minute = (i % 2) * 30
-
     const timeSlotDate = new Date(
       date.getFullYear(),
       date.getMonth(),
@@ -25,16 +18,21 @@ const DayColumn = ({ date }: DayColumnProps) => {
       hour,
       minute
     )
-
-    let isFutureTimeSlot = false
-    if (isToday) {
-      isFutureTimeSlot = isAfter(timeSlotDate, now)
-    } else {
-      isFutureTimeSlot = isAfter(timeSlotDate, startOfDay(now))
-    }
+    const isToday = isSameDay(date, now)
+    const isFutureTimeSlot = isToday
+      ? isAfter(timeSlotDate, now)
+      : isAfter(timeSlotDate, startOfDay(now))
 
     return { timeSlotDate, isFutureTimeSlot }
   })
+
+const DayColumn = ({ date }: DayColumnProps) => {
+  const now = new Date()
+  const dayOfWeek = format(date, 'eee', { locale: ko })
+  const dayOfMonth = format(date, 'd')
+  const isToday = isSameDay(date, now)
+  const isHoliday = ['토', '일'].includes(dayOfWeek)
+  const timeSlots = calculateTimeSlots(date, now)
 
   return (
     <div className='flex-1 flex flex-col'>
